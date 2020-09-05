a3150ce82e31e95ef62ed57c0d773417d983ddbe
stuti135 <62691759+stuti135@users.noreply.github.com>
2020-09-06 02:35:43 +0530

    Add files via upload

---
+++ ngo-management-system-master/README.md
@@ -0,0 +1,2 @@
+# ngo-management-system
+orphanage home
---
+++ ngo-management-system-master/orphan/Orphanage/resources/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.js--wp-1').waypoint(function(direction){
+		$('.js--wp-1').addClass('animate__animated animate__pulse');
+	});
+	
+	$('.js--wp-4').waypoint(function(direction){
+		$('.js--wp-4').addClass('animate__animated animate__pulse');
+	});
+	
+	$('.js--wp-5').waypoint(function(direction){
+		$('.js--wp-5').addClass('animate__animated animate__pulse');
+	});
+	
+	$('.js--wp-6').waypoint(function(direction){
+		$('.js--wp-6').addClass('animate__animated animate__pulse');
+	});
+});
